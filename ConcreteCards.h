827d833a8d157262eac8f0fa42120195721c4a8e
TeaSings <teasings@qq.com>
2026-02-18 17:25:55 +0800

    Applied Prototype Design

---
+++ ConcreteCards.h
@@ -0,0 +1,51 @@
+#ifndef CARD_BASIC_H
+#define CARD_BASIC_H
+
+#include "Card.h"
+
+class StrikeCard :public Card {
+public:
+    StrikeCard (const std::string& name = "", const int cost = 0)
+    : Card(name, cost)
+    {}
+
+    virtual void play () override {
+        std::cout << "造成 6 点伤害" << std::endl;
+    }
+
+    virtual Card* clone () const override {
+        return new StrikeCard(*this);
+    }
+};
+
+class DefendCard :public Card {
+public:
+    DefendCard (const std::string& name = "", const int cost = 0)
+    : Card(name, cost)
+    {}
+
+    virtual void play () override {
+        std::cout << "获得 5 点格挡" << std::endl;
+    }
+
+    virtual Card* clone () const override {
+        return new DefendCard(*this);
+    }
+};
+
+class BashCard :public Card {
+public:
+    BashCard (const std::string& name = "", const int cost = 0)
+    : Card(name, cost)
+    {}
+
+    virtual void play() override {
+        std::cout << "造成 8 点伤害，并给予 1 层易伤" << std::endl;
+    }
+
+    virtual Card* clone () const override {
+        return new BashCard(*this);
+    }
+};
+
+#endif
